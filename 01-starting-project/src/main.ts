c55a848ce0f8637044ee38b581fe6d5152a38a31
Louis <LouisLo@YUCHENS.COM>
2025-09-12 16:49:15 +0800

    Add Navbar Component

--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './app/components/header/header.component';
+import { NavbarComponent } from './app/components/navbar/navbar.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(HeaderComponent).catch((err) => console.error(err));
+bootstrapApplication(NavbarComponent).catch((err) => console.error(err));
