999e67a6043faea3fc5e2d49f508ac2b043a8db4
Louis <LouisLo@YUCHENS.COM>
2025-09-15 09:44:55 +0800

    update

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- 01-starting-project/src/main.ts
+++ 01-starting-project/src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/components/header/header.component';
-import { NavbarComponent } from './app/components/navbar/navbar.component';
+
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
-bootstrapApplication(HeaderComponent).catch((err) => console.error(err));
-bootstrapApplication(NavbarComponent).catch((err) => console.error(err));
